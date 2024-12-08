ffae236a0875421a25b1f9c261db2be2f3439aad
Teddy Paul <tdy.paul@gmail.com>
2024-12-08 22:29:45 +0100

    fix: build errors

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,19 +1,28 @@
+import About from '../pages/About.vue';
+import Experiences from '../pages/Experiences.vue';
+import Home from '../pages/Home.vue';
+import Projects from '../pages/Projects.vue';
+
 export const routes = [
     {
         path: '/',
         name: 'Home',
+        component: Home,
     },
     {
         path: '/#about',
         name: 'À propos',
+        component: About,
     },
     {
         path: '/#projects',
         name: 'Projets',
+        component: Projects,
     },
     {
         path: '/#experiences',
         name: 'Expériences',
+        component: Experiences,
     },
     // {
     //     path: '/#contact',
