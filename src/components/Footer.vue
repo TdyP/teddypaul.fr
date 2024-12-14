063c6d3f85288a961409069a5ee2ddc071fcdfdf
Teddy Paul <tdy.paul@gmail.com>
2024-12-14 22:58:24 +0100

    Fix footer on mobile

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,12 +3,12 @@ import SocialLinks from './SocialLinks.vue';
 </script>
 
 <template>
-    <footer class="bg-orange-400 text-center py-4">
+    <footer class="bg-orange-400 text-center p-4">
         <SocialLinks class="justify-center inline" />
         <p>Teddy Paul - Développeur freelance
         </p>
         <p class="mt-[1em]">Site web réalisé avec VueJS et TailwindCSS.
-            Code source disponible sur <a href=" https://github.com/TdyP/teddypaul.fr">GitHub</a>
+            Code source disponible sur <a href="https://github.com/TdyP/teddypaul.fr">GitHub</a>
         </p>
     </footer>
 </template>
